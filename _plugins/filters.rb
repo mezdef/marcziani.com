922fb6aea040ec20cc21e3ff725ccb7871295060
Marc Ziani de Ferranti <mezdef@gmail.com>
2014-02-18 15:35:52 +1100

    Fix - Posts: Updating markdown filter to function with kramdown instead of redcarpet

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,8 +1,8 @@
 module Jekyll
   module MarkdownFixes
     def stripem(input)
-      m = input.gsub(/<p><img(.*?)>\s<figcaption(.*?)<\/figcaption><\/p>/, "<figure><img \\1><figcaption \\2 </figcaption></figure>")
-      n = m.gsub(/<p><img(.*?)>\s<img(.*?)>\s<figcaption(.*?)<\/figcaption><\/p>/, "<figure><img \\1><img \\2><figcaption \\3 </figcaption></figure>")
+      m = input.gsub(/<p class="md-figure">(.*?)\/>\s(.*?)<\/p>/, "<figure>\\1 /><figcaption>\\2</figcaption></figure>")
+      n = m.gsub(/<p class="md-figure">(.*?)\/>\s(.*?)\/>\s(.*?)<\/p>/, "<figure><img \\1><img \\2><figcaption>\\3</figcaption></figure>")
       return n
     end
   end
