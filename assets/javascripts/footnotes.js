2297eb88837aea829cba6909c3ee20eb7b4de49f
Marc Ziani de Ferranti <marc.z@samsung.com>
2016-07-20 16:52:45 -0400

    Feature - Post: added basic pop-up footnotes

---
+++ assets/javascripts/footnotes.js
@@ -0,0 +1,33 @@
+var footnote = document.createElement('div');
+var fnClass = "fn-container";
+var fnSup= "sup[id^='fnref']";
+var fnContainer = document.getElementsByClassName(fnClass);
+var fnTargets = document.querySelectorAll(fnSup + ' a');
+footnote.classList.add("fn-container");
+
+
+// # Listen for clicks outside the footnote
+document.addEventListener('click', function(event) {
+  if (fnContainer.length > 0) {
+    if (event.target.className !== fnClass && event.target.className !== 'footnote') {
+      fnContainer[0].parentNode.removeChild(fnContainer[0]);
+      document.documentElement.classList.remove('fn-open');
+    }
+  }
+});
+
+// # Listen on every footnote
+for (i = 0; i < fnTargets.length; i++) {
+  fnTargets[i].addEventListener('click', function(target) {
+    target.preventDefault();
+    fnCreate(target)
+  });
+}
+
+// # Insert Footnote
+function fnCreate(target) {
+  var targetId = target.srcElement.text;
+  footnote.innerHTML = document.getElementById('fn:' + targetId).children[0].innerHTML;
+  target.srcElement.parentNode.appendChild(footnote);
+  document.documentElement.classList.add('fn-open');
+}
