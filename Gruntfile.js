6b64640deae641f0301c05322ef3592758e9ceca
Marc Ziani de Ferranti <marc.z@samsung.com>
2016-07-22 18:00:26 -0400

    WIP - Workflow: something seems to have gone wrong with Jekyll index files?

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,38 @@
 'use strict';
 module.exports = function(grunt) {
+  // require('time-grunt')(grunt);
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // # Plugin: Shell
+    // # Plugin: Shell tasks
     shell: {
-      clean: {
-        command: 'rm -rf .tmp .jekyll _site',
-      },
       // sync: { command: 's3_website push', },
-    },
-    clean: ['.tmp', '.jekyll', '_site'],
-
-    // # Plugin: Watch
-    watch: {
-      less: {
-        files: ['assets/**/*.less'],
-        tasks: ['less:server', 'postcss:server'],
-      },
-      scripts: {
-        files: ['assets/**/*.js'],
-        tasks: ['uglify:server'],
-      },
-      jekyll: {
-        files: ['**/*.{html,haml,md,mkd,markdown,rb,svg,xml,yml}', '!_site/**/.{html,md,rb,svg,xml,yml}'],
-        tasks: ['jekyll:server'],
-      },
+      dir: { command: 'mkdir .tmp .jekyll _site', },
+      clean: { command: 'rm -rf .tmp .jekyll _site', },
     },
 
-    // # Plugin: Jekyll
+    // # Plugin: Jekyll HTML generation
     jekyll: {
       options: {
-        // config: '_config.yml,_config.build.yml',
         src: './',
+        config: '_config.yml',
+        // quiet: true, drafts: true, future: true,
+        // incremental: true,
       },
-      server: {
-        options: {
-          config: '_config.yml',
-          dest: '.jekyll/',
-          // quiet: true,
-          // incremental: true,
-        },
-      },
-      dist: {
-        options: {
-          dest: '_site',
-          quiet: true,
-        },
-      },
-      layout: {
-        options: {
-          config: '_config.yml',
-          dest: '.jekyll/',
-          quiet: true,
-          // incremental: true,
-        },
-      },
+      server: { options: { dest: '.jekyll/', }, },
+      dist: { options: { dest: '_site/', }, },
     },
 
-    // # Plugin: Less
+    // # Plugin: Less processing
     less: {
-      server: {
-        options: {
-          paths: ['assets/'],
-        },
-        files: { '.tmp/assets/screen.min.css': 'assets/_screen.less' },
+      options: {
+        paths: ['assets/'],
       },
-      dist: {
-        options: {
-          paths: ['assets/'],
-          plugins: [
-          ],
-        },
-        // compilation.css  :  source.less
-        files: { '_site/assets/screen.min.css': 'assets/_screen.less' },
-      },
+      server: { files: { '.tmp/assets/screen.min.css': 'assets/_screen.less' }, },
+      dist: { files: { '_site/assets/screen.min.css': 'assets/_screen.less' }, },
     },
 
+    // # Plugin: Minify CSS
     postcss: {
       options: {
         processors: [
@@ -86,58 +42,69 @@ module.exports = function(grunt) {
               '>0.1%', 'ie 8', 'chrome 6', 'firefox 20', 'safari 4', 'opera 12',
               'android 4', 'ios_saf 4', 'op_mini all', 'samsung 4',
             ],
-          }), // add vendor prefixes
+          }),
           // require('cssnano')() // minify the result
         ]
       },
-      server: {
-        src: '.tmp/assets/screen.min.css'
-      },
-      dist: {
-        src: '_site/assets/screen.min.css'
-      },
+      server: { src: '.tmp/assets/screen.min.css' },
+      dist: { src: '_site/assets/screen.min.css' },
     },
-
-    // csslint: {
-    //   src: ['.tmp/assets/screen.min.css']
-    // },
-    //
-    // lesslint: {
-    //   options: {
-    //     imports: ['assets'],
-    //   },
-    //   src: ['assets/**/*.less'],
-    // },
 
     // # Plugin: Minify JS
     uglify: {
-      options: {
-        preserveComments: false,
-      },
+      options: { preserveComments: false, },
       server: {
         files: [{
-          expand: true, // Extensions in filenames begin after the first dot
-          src: ['assets/js/*.js'], // Extensions in filenames begin after the first dot
-          dest: '.tmp/', // Extensions in filenames begin after the first dot
-          ext: '.min.js', // Extensions in filenames begin after the first dot
-          extDot: 'first', // Extensions in filenames begin after the first dot
+          expand: true,
+          src: ['assets/js/*.js'], ext: '.min.js',
+          dest: '.tmp/',
         }],
       },
       dist: {
         files: [{
           expand: true,
-          cwd: 'assets/js/',
-          src: ['**/*.js'],
-          dest: '_site/assets/js/',
-          ext: '.min.js',
-          extDot: 'first',
+          src: ['assets/js/*.js'], ext: '.min.js',
+          dest: '_site/',
         }],
       },
     },
 
-    // # Task: Html Minification
+    // # Plugin: Minify Images
+    imagemin: {
+      options: {
+        optimizationLevel: 3,
+        svgoPlugins: [{ removeViewBox: false }],
+        // use: [mozjpeg()]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          src: ['assets/img/*.{png,jpg,gif}'],
+          dest: '_site/'
+        }],
+      },
+    },
+
+    // # Plugin: Copy files
+    copy: {
+      server: {
+        expand: true,
+        cwd: '_site/',
+        src: '**',
+        dest: '.tmp/',
+      },
+      dist: {
+        files: [{
+          expand: true,
+          src: [ 'assets/img/favicon.{ico,png}' ],
+          dest: '_site/',
+        }],
+      },
+    },
+
+    // // # Plugin: Minify Html
     // htmlmin: {                                     // Task
-    //   dist: {                                      // Target
+    //   server: {                                      // Target
     //     options: {                                 // Target options
     //       removeComments: true,
     //       collapseWhitespace: true,
@@ -150,60 +117,73 @@ module.exports = function(grunt) {
     //     },
     //     files: [{
     //       expand: true,
-    //       cwd: '_site',
-    //       src: '**/*.html',
-    //       dest: '_site',
+    //       // src: '.jekyll/**/*.html',
+    //       // dest: './',
+    //       src: '.jekyll/**/*.html',
+    //       dest: './',
     //     }],
     //   },
     // },
 
-    // # Plugin: Copy files
-    copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: './',
-          src: [
-            // Jekyll processes and moves HTML and text files.
-            // Usemin moves CSS and javascript inside of Usemin blocks.
-            // Copy moves asset files and directories.
-            'img/**/*',
-            // Like Jekyll, exclude files & folders prefixed with an underscore.
-            '!**/_*{,/**}',
-            // Explicitly add any files your site needs for distribution here.
-            'favicon*.{ico,png}'
-          ],
-          dest: '_site'
-        }]
+    // # Plugin: Watch
+    watch: {
+      less: {
+        files: ['assets/**/*.less'],
+        tasks: ['less:server', 'postcss:server'],
       },
-    },
-
-    concurrent: {
-      server: [ 'less', 'jekyll:server' ],
-      dist: [ 'less', 'copy:dist' ],
+      scripts: {
+        files: ['assets/**/*.js'],
+        tasks: ['uglify:server'],
+      },
+      jekyll: {
+        files: [
+          '**/*.{haml,md,mkd,markdown,rb,svg,xml,yml}',
+          // '!tmp/**/*.{html,haml,md,mkd,markdown,rb,svg,xml,yml}',
+          // '!_site/**/.{html,md,rb,svg,xml,yml}',
+          // '!.tmp/**/.{html,md,rb,svg,xml,yml}',
+          '!tmp/**',
+          '!.tmp/**',
+          '!.jekyll/**',
+          '!_site/**',
+          '!node_modules/**',
+          // '!node_modules/**/.{html,md,rb,svg,xml,yml}',
+        ],
+        tasks: ['jekyll:server'],
+      },
     },
 
     // # Plugin: Browser Sync
     browserSync: {
+      options: {
+        watchTask: true,
+        browser: "google chrome canary",
+      },
       server: {
         bsFiles: {
           src: [
-            '.jekyll/**/*.html',
-            '.tmp/assets/*.css',
-            '.tmp/assets/js/*.min.js',
+            '.jekyll/**/*.html', '!.jekyll/tmp/**/*.html',
+            '.tmp/assets/*.css', '.tmp/assets/js/*.min.js',
             '.tmp/assets/**/*.{gif,jpg,jpeg,png,svg}',
-          ]
+          ],
         },
         options: {
-          // baseDir: [ '.jekyll', '.tmp',],
-          browser: "google chrome canary",
-          watchTask: true,
-          server: ['.tmp', '.jekyll'],
+          server: ['.tmp/', '.jekyll/'],
+          // injectChanges: true,
         },
       },
+      // dist: {
+      //   bsFiles: {
+      //     src: [
+      //       '_site/**/*.html', '!_site/tmp/**/*.html',
+      //       '_site/assets/*.css', '_site/assets/js/*.min.js',
+      //       '_site/assets/**/*.{gif,jpg,jpeg,png,svg}',
+      //     ],
+      //   },
+      //   options: {
+      //     server: '_site/',
+      //   },
+      // },
     },
-
 
     // confirm: {
     //   sync: {
@@ -213,40 +193,36 @@ module.exports = function(grunt) {
     //     }
     //   }
     // }
+
+    // csslint: {
+    //   src: ['.tmp/assets/screen.min.css']
+    // },
+    //
+    // lesslint: {
+    //   options: {
+    //     imports: ['assets'],
+    //   },
+    //   src: ['assets/**/*.less'],
+    // },
+
+    // # Plugin: Concurrent Tasks
+    concurrent: {
+      dist: ['less:dist', 'uglify:dist', 'imagemin:dist', 'copy:dist'],
+    },
+
   });
   require('load-grunt-tasks')(grunt);
 
-  // Task: Serve
-  grunt.registerTask('serve', function(target) {
-    if (target === 'dist') { return grunt.task.run(['build', 'browserSync:dist']); }
-    grunt.task.run([
-      'shell:clean',
-      'clean',
-      'jekyll:server',
-      'less:server',
-      'uglify:server',
-      'browserSync:server',
-      'watch',
-    ]);
-  });
-
   // Task: Build
   grunt.registerTask('build', [
-    'shell:clean',
-    'clean',
+    'shell:clean', 'shell:dir',
     'jekyll:dist',
-    'less:dist',
+    'concurrent:dist',
     'postcss:dist',
-    'less:dist',
-    'copy:dist',
-    'uglify:dist',
-    // 'htmlmin',
-    // 'critical',
-    // 'imagemin',
-    // 'svgmin',
   ]);
 
-  // grunt.registerTask('dev', ['clean:dist', 'jekyll:dist', 'less', 'uglify:dist', 'concurrent:all']);
-  // grunt.registerTask('confirm', ['confirm:sync']);
-  // grunt.registerTask('sync', ['clean:dist', 'jekyll:dist', 'less', 'shell:sync']);
+  // Task: Development
+  grunt.registerTask('dev', ['build', 'copy:server', 'browserSync:server', 'watch']);
+  grunt.registerTask('test', ['build', 'browserSync:dist', 'watch']);
+  // grunt.registerTask('sync', ['build', 'shell:sync']);
 };
