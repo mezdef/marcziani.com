7f69ee1b34463ff8c9ab8d8d82da267df92250e7
Marc Ziani de Ferranti <marc.z@samsung.com>
2016-08-03 15:51:24 -0400

    Update - Post: added lazy loading for images

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,8 +1,8 @@
 module Jekyll
   module MarkdownFixes
     def stripem(input)
-      m = input.gsub(/<p class="md-figure">(.*?)\/>\s(.*?)<\/p>/, "<figure>\\1 /><figcaption>\\2</figcaption></figure>")
-      n = m.gsub(/<p class="md-figure">(.*?)\/>\s(.*?)\/>\s(.*?)<\/p>/, "<figure><img \\1><img \\2><figcaption>\\3</figcaption></figure>")
+      m = input.gsub(/<p class="md-figure"><img src="(.*?)"(.*?)\/>\s(.*?)<\/p>/, "<figure><img class='lzy' data-src='\\1' \\2/><figcaption>\\3</figcaption></figure>")
+      n = m.gsub(/<p class="md-figure"><img src="(.*?)"(.*?)\/>\s<img src="(.*?)"(.*?)\/>\s(.*?)<\/p>/, "<figure class='grid'><picture><img class='lzy' data-src='\\1' \\2></picture><picture><img class='lzy' data-src='\\3' \\4></picture><figcaption>\\5</figcaption></figure>")
       return n
     end
   end
