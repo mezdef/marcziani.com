597a30b3963297d10263dfd8fc73e0338b1a424a
Marc Ziani de Ferranti <marc.z@samsung.com>
2016-10-11 15:32:27 -0400

    Feature - Credits: adding filter for Markdown credits

--- _plugins/filters.rb
+++ _plugins/filters.rb
@@ -1,6 +1,6 @@
 require 'nokogiri'
-
 module Jekyll
+
   module MarkdownFixes
     def stripem(input)
       m = input.gsub(/<p class="md-figure"><img src="(.*?)"(.*?)\/>\s(.*?)<\/p>/, "<figure><img class='lzy' data-src='\\1' \\2/><figcaption>\\3</figcaption></figure>")
@@ -34,10 +34,26 @@ module Jekyll
         gallery.children.first.children.first["src"] = lzyUri
         gallery.children.first.children.first["data-src"] = firstUrl
       end
-
       doc.to_html
     end
   end
+
+  module CreditsConstruction
+    def credits(input)
+      doc = Nokogiri::HTML.fragment(input)
+      doc.css(".md-credits").each_with_index do |credit, index|
+        credit["class"] = "credits"
+        # heading = credits.previous_element
+        # credits.children.first.add_previous_sibling(heading)
+        credit.css("li strong").each_with_index do |strong, index|
+          strong.name = "label"
+        end
+      end
+      doc.to_html
+    end
+  end
+
 end
 Liquid::Template.register_filter(Jekyll::MarkdownFixes)
 Liquid::Template.register_filter(Jekyll::GalleryConstruction)
+Liquid::Template.register_filter(Jekyll::CreditsConstruction)
